6e6c6a386a4e75d3e528de7e7e5fbafa859fe801
Pablo <seguiaguilarpablo@gmail.com>
2023-03-12 15:31:50 +0100

    a

--- users/pablo/programs/firefox/default.nix
+++ users/pablo/programs/firefox/default.nix
@@ -8,30 +8,11 @@
   programs.firefox = {
     enable = true;
 
-    profiles.alpha = {
+    profiles.pablo = {
       id = 0;
       settings."general.smoothScroll" = true;
       userChrome = import ./userChrome-css.nix {inherit colors;};
       userContent = import ./userContent-css.nix {};
-      extensions = with nur.repos.rycee.firefox-addons; [
-        enhanced-github
-        enhancer-for-youtube
-        refined-github
-        stylus
-        vue-js-devtools
-        react-devtools
-        reduxdevtools
-      ];
-      extraConfig = ''
-        user_pref("browser.urlbar.autoFill", false);
-        user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-        user_pref("layers.acceleration.force-enabled", true);
-        user_pref("gfx.webrender.all", true);
-        user_pref("svg.context-properties.content.enabled", true);
-        user_pref("full-screen-api.ignore-widgets", true);
-        user_pref("media.ffmpeg.vaapi.enabled", true);
-        user_pref("media.rdd-vpx.enabled", true);
-      '';
     };
   };
 }
