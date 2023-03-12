ab07a1813c9c1b18d7275c0f8fec992e67d5c6bd
Pablo <seguiaguilarpablo@gmail.com>
2023-03-12 19:57:34 +0100

    .

--- users/pablo/programs/vscode/default.nix
+++ users/pablo/programs/vscode/default.nix
@@ -1,9 +1,6 @@
 { config, pkgs }:
 
 {
-  home.packages = with pkgs; [
-    luaFormatter
-  ];
 
   programs.vscode = {
     enable = true;
@@ -16,9 +13,6 @@
       "editor.cursorStyle" = "line-thin";
       "editor.fontSize" = 16;
       "terminal.integrated.fontSize" = 16;
-      "editor.defaultFormatter" = "Koihik.vscode-lua-format";
-      "vscode-lua-format.binaryPath" = "${pkgs.luaFormatter}/bin/lua-format";
-      "vscode-lua-format.configPath" = "${config.xdg.configHome}/LuaFormatter.cfg";
       "window.menuBarVisibility" = "toggle";
       "editor.tabSize" = 2;
       "editor.inlineSuggest.enabled" = true;
@@ -26,24 +20,11 @@
     };
     extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
+      
       esbenp.prettier-vscode
       naumovs.color-highlight
-      svelte.svelte-vscode
       ms-vsliveshare.vsliveshare
-      github.copilot
     ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "volar";
-        publisher = "vue";
-        version = "1.0.12";
-        sha256 = "sha256-D9E3KRUOlNVXH4oMv1W0+/mbqO8Se7+6E2F5P/KvCro=";
-      }
-      {
-        name = "vscode-typescript-vue-plugin";
-        publisher = "vue";
-        version = "1.0.12";
-        sha256 = "sha256-WiL+gc9+U861ubLlY/acR+ZcrFT7TdIDR0K1XNNidX8=";
-      }
       {
         name = "decay";
         publisher = "decaycs";
@@ -56,22 +37,7 @@
         version = "4.22.0";
         sha256 = "sha256-U9P9BcuZi+SUcvTg/fC2SkjGRD4CvgJEc1i+Ft2OOUc=";
       }
-      {
-        name = "vscode-lua-format";
-        publisher = "koihik";
-        version = "1.3.8";
-        sha256 = "sha256-ACdjiy+Rj2wmxvSojaJmtCwyryWWB+OA/9hBEMJi39g=";
-      }
     ];
   };
 
-  xdg.configFile."LuaFormatter.cfg".text = ''
-    indent_width: 2
-    use_tab: false
-    keep_simple_control_block_one_line: false
-    keep_simple_function_one_line: false
-    single_quote_to_double_quote: true
-    chop_down_table: true
-    chop_down_kv_table: true
-  '';
 }
