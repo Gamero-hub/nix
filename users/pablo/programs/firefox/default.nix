a2b3ac3f71f0e12ce0fe199dbeb9617b2a8e3af2
Pablo <seguiaguilarpablo@gmail.com>
2023-03-13 15:14:13 +0100

    .

--- users/pablo/programs/firefox/default.nix
+++ users/pablo/programs/firefox/default.nix
@@ -1,25 +1,34 @@
-{ config, pkgs, nur, colors, ... }:
+{ config, pkgs, ... }:
 
 {
   programs.firefox = {
     enable = true;
-     
-    profiles.pablo = {
-      id = 0;
-      settings."general.smoothScroll" = true;
-      userChrome = import ./userChrome-css.nix { inherit colors; };
-      userContent = import ./userContent-css.nix {};
-      extraConfig = ''
-        user_pref("browser.startup.homepage", "https://gamero-hub.github.io/page");
-        user_pref("browser.urlbar.autoFill", false);
-        user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-        user_pref("layers.acceleration.force-enabled", true);
-        user_pref("gfx.webrender.all", true);
-        user_pref("svg.context-properties.content.enabled", true);
-        user_pref("full-screen-api.ignore-widgets", true);
-        user_pref("media.ffmpeg.vaapi.enabled", true);
-        user_pref("media.rdd-vpx.enabled", true);
-      '';
+
+    profiles = {
+      myprofile = {
+        id = 0;
+
+        settings = {
+          "browser.startup.homepage" = "https://gs.is-a.dev/startpage/";
+          "general.smoothScroll" = true;
+        };
+
+        userChrome = import ./userChrome-css.nix {
+          theme = config.colorScheme;
+        };
+
+        userContent = import ./userContent-css.nix {
+          theme = config.colorScheme;
+        };
+
+        extraConfig = ''
+          user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+          user_pref("full-screen-api.ignore-widgets", true);
+          user_pref("media.ffmpeg.vaapi.enabled", true);
+          user_pref("media.rdd-vpx.enabled", true);
+          user_pref("extensions.pocket.enabled", false);
+        '';
+      };
     };
   };
 }
