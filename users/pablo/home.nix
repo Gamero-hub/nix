b6b9f8dbfc65baedf6877796148eb2cb4cbcb8ee
Pablo <seguiaguilarpablo@gmail.com>
2023-03-13 16:06:01 +0100

    .

--- users/pablo/home.nix
+++ users/pablo/home.nix
@@ -65,7 +65,7 @@ in {
       (import ./programs/rofi.nix {inherit pkgs config lib;})
       (import ./programs/fish.nix {inherit pkgs;})
       (import ./programs/kitty)
-      (import ./programs/firefox {inherit pkgs config colors;})
+      (import ./programs/firefox {inherit pkgs config colors extensions;})
       (import ./programs/bspwm {inherit pkgs;})
       (import ./programs/starship.nix)
       (import ./programs/git {inherit pkgs lib config;})
