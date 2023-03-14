3845c81b49f0a294bac83dfd38a9014271d021f2
Pablo <seguiaguilarpablo@gmail.com>
2023-03-14 14:38:36 +0100

    .

--- users/pablo/programs/kitty/default.nix
+++ users/pablo/programs/kitty/default.nix
@@ -3,7 +3,7 @@
 }:{
     programs.kitty = {
         enable = true;
-	    extraConfig = import ./colors/decayce.nix;
+	    extraConfig = import ./colors/oled_mocha.nix;
 	    settings = {
             font_size = "15";
 	        font_family = "Iosevka Term Nerd Font Complete Medium Italic";
