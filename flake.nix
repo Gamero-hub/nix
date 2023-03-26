63e697faeb35f953473620da57542abfb42a4acc
Pablo <seguiaguilarpablo@gmail.com>
2023-03-26 22:29:56 +0200

    .

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
+    webcord.url = "github:fufexan/webcord-flake";
     nix-gaming.url = github:fufexan/nix-gaming;
     nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
