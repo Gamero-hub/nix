3f0dd630f49b43fe72288d8e03883b5f5e9d20a3
Gamero-xD <109750172+Gamero-hub@users.noreply.github.com>
2023-05-21 16:18:39 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,12 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     spicetify-nix.url = "github:the-argus/spicetify-nix";
     home-manager = {
-      url = github:nix-community/home-manager;
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
   
-
   outputs = { self, nixpkgs, home-manager, spicetify-nix, ... }: 
     let
       system = "x86_64-linux"; 
@@ -23,52 +22,55 @@
       nixosConfigurations = {
         virtland = lib.nixosSystem {
           inherit system;
-          modules = [ 
-          ./hosts/virtland/configuration.nix 
-          ./hosts/nixstuff
-          home-manager.nixosModules.home-manager {
-           home-manager.useGlobalPkgs = true;
-           home-manager.useUserPackages = true;
-           home-manager.users.pablo = {
-             imports = [ ./users/pablo/home.nix
-                         ./users/pablo/programs/spicetify-nix.nix
-            ];
-          };
-        }
+          modules = [
+            ./hosts/virtland/configuration.nix 
+            ./hosts/nixstuff
+            (home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.pablo = {
+                imports = [
+                  ./users/pablo/home.nix
+                  ./users/pablo/programs/spicetify-nix.nix
+                ];
+              };
+            })
           ];
         };
         lowland = lib.nixosSystem {
           inherit system;
-          modules = [ 
-          ./hosts/lowland/configuration.nix 
-          ./hosts/nixstuff
-          home-manager.nixosModules.home-manager {
-           home-manager.useGlobalPkgs = true;
-           home-manager.useUserPackages = true;
-           home-manager.users.pablo = {
-             imports = [ ./users/pablo/home.nix
-                         ./users/pablo/programs/spicetify-nix.nix
-            ];
-          };
-        }
+          modules = [
+            ./hosts/lowland/configuration.nix 
+            ./hosts/nixstuff
+            (home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.pablo = {
+                imports = [
+                  ./users/pablo/home.nix
+                  ./users/pablo/programs/spicetify-nix.nix
+                ];
+              };
+            })
           ];
-        };           
+        };
         highland = lib.nixosSystem {
           inherit system;
-          modules = [ 
-          ./hosts/highland/configuration.nix 
-          ./hosts/nixstuff
-          home-manager.nixosModules.home-manager {
-           home-manager.useGlobalPkgs = true;
-           home-manager.useUserPackages = true;
-           home-manager.users.pablo = {
-             imports = [ ./users/pablo/home.nix
-                         ./users/pablo/programs/spicetify-nix.nix
-             ];
-          };
-        }
+          modules = [
+            ./hosts/highland/configuration.nix 
+            ./hosts/nixstuff
+            (home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.pablo = {
+                imports = [
+                  ./users/pablo/home.nix
+                  ./users/pablo/programs/spicetify-nix.nix
+                ];
+              };
+            })
           ];
-        };           
-};
+        };
       };
-  }
+    };
+}
