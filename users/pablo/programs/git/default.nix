0996a786c3f69ebacc413f61e7260e5a8a70b8c3
Pablo Segui <seguiaguilarpablo@gmail.com>
2023-06-10 21:40:56 +0200

    .

--- users/pablo/programs/git/default.nix
+++ users/pablo/programs/git/default.nix
@@ -6,4 +6,10 @@
     userName = "Pablo Segui";
     userEmail = "seguiaguilarpablo@gmail.com";
   };
+  extraConfig = {
+    credential = {
+      credentialStore = "secretservice";
+      helper = "/nix/store/5834mf7rd834g9panbqrl4z3r3dkfg8i-gh-2.20.2/bin/gh";
+    };
+  };
 }
