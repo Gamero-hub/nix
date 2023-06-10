3c7d2de5f42fd6c22c8325d08a1296936ed3b687
Pablo Segui <seguiaguilarpablo@gmail.com>
2023-06-10 16:15:11 +0200

    .

--- users/pablo/programs/spicetify.nix
+++ users/pablo/programs/spicetify.nix
@@ -3,28 +3,22 @@ let
   flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
   spicetify-nix = (import flake-compat { src = builtins.fetchTarball "https://github.com/the-argus/spicetify-nix/archive/master.tar.gz"; }).defaultNix;
   spicePkgs = spicetify-nix.packages.${pkgs.system}.default;
+  
+
+  officialThemesOLD = pkgs.fetchgit {
+      url = "https://github.com/spicetify/spicetify-themes";
+      rev = "c2751b48ff9693867193fe65695a585e3c2e2133";
+      sha256 = "0rbqaxvyfz2vvv3iqik5rpsa3aics5a7232167rmyvv54m475agk";
+    };
+    localFilesSrc = pkgs.fetchgit {
+      url = "https://github.com/hroland/spicetify-show-local-files/";
+      rev = "1bfd2fc80385b21ed6dd207b00a371065e53042e";
+      sha256 = "01gy16b69glqcalz1wm8kr5wsh94i419qx4nfmsavm4rcvcr3qlx";
+    };
 in
-
 {
-    imports = [spicetify-nix.homeManagerModule];
-  programs.spicetify =
-      let
-      # use a different version of spicetify-themes than the one provided by
-      # spicetify-nix
-      officialThemesOLD = pkgs.fetchgit {
-        url = "https://github.com/spicetify/spicetify-themes";
-        rev = "c2751b48ff9693867193fe65695a585e3c2e2133";
-        sha256 = "0rbqaxvyfz2vvv3iqik5rpsa3aics5a7232167rmyvv54m475agk";
-      };
-      # pin a certain version of the localFiles custom app
-      localFilesSrc = pkgs.fetchgit {
-        url = "https://github.com/hroland/spicetify-show-local-files/";
-        rev = "1bfd2fc80385b21ed6dd207b00a371065e53042e";
-        sha256 = "01gy16b69glqcalz1wm8kr5wsh94i419qx4nfmsavm4rcvcr3qlx";
-      };
-    in
-
-   {
+  imports = [spicetify-nix.homeManagerModule];
+  programs.spicetify = {
     enable = true;
     enabledExtensions = with spicePkgs.extensions; [
         playlistIcons
