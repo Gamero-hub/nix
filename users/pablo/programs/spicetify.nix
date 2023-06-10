994904c642e9ef54f23f0a21d9dbe13d6dba2e91
Pablo Segui <seguiaguilarpablo@gmail.com>
2023-06-10 16:23:38 +0200

    .

--- users/pablo/programs/spicetify.nix
+++ users/pablo/programs/spicetify.nix
@@ -1,4 +1,4 @@
-{spicetify-nix,  pkgs, ...}:
+{wave, spicetify-nix,  pkgs, ...}:
 let
   flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
   spicetify-nix = (import flake-compat { src = builtins.fetchTarball "https://github.com/the-argus/spicetify-nix/archive/master.tar.gz"; }).defaultNix;
@@ -56,7 +56,24 @@ in
         overwriteAssets = true;
         sidebarConfig = true;
       };
- 
+      customColorScheme = with colors;{
+        text = "${color7}";
+        subtext = "${color15}";
+        sidebar-text = "${color7}";
+        main = "${background}";
+        sidebar = "${mbg}";
+        player = "${bg2}";
+        card = "${color0}";
+        shadow = "${color8}";
+        selected-row = "${color8}";
+        button = "${color4}";
+        button-active = "${color4}";
+        button-disabled = "${color5}";
+        tab-active = "${color4}";
+        notification = "${color3}";
+        notification-error = "${color1}";
+        misc = "${comment}";
+      };
       
 
   };
