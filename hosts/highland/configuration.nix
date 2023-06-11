6f83d523c75281fde3036edc33ed0699cf8bdf8c
Pablo Segui <seguiaguilarpablo@gmail.com>
2023-06-11 15:02:58 +0200

    .

--- hosts/highland/configuration.nix
+++ hosts/highland/configuration.nix
@@ -9,6 +9,7 @@
     programs.nix-ld.enable = true;
 
   #Bootloader
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
