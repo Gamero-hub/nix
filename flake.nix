47f4131a1219ebec6141931ae2cd7c94a40a5f86
Pablo <seguiaguilarpablo@gmail.com>
2023-07-12 20:22:03 +0200

    .

--- flake.nix
+++ flake.nix
@@ -1,54 +1,40 @@
 {
   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-stable";
-
-    home-manager = {
-      url = github:nix-community/home-manager;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
-    nixpkgs-f2k.url = "github:fortuneteller2k/nixpkgs-f2k";
-    nur.url = github:nix-community/NUR;
-
-    hyprland = {
-      url = github:hyprwm/Hyprland;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    sf-mono-liga-src = {
-      url = "github:shaunsingh/SFMono-Nerd-Font-Ligaturized";
-      flake = false;
-    };
-
-    st = {
-      url = github:AlphaTechnolog/st;
-      flake = false;
-    };
-
-    luaFormatter = {
-      type = "git";
-      url = "https://github.com/Koihik/LuaFormatter.git";
-      submodules = true;
-      flake = false;
-    };
-  
+   inputs = {
+    master.url = "github:nixos/nixpkgs/master";
+    stable.url = "github:nixos/nixpkgs/nixos-22.11";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    nur.url = "github:nix-community/NUR";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     spicetify-nix.url = "github:the-argus/spicetify-nix";
+    nixpkgs-f2k.url = "github:fortuneteller2k/nixpkgs-f2k";
     # Channel to follow.
+    home-manager.inputs.nixpkgs.follows = "unstable";
+    nixpkgs.follows = "unstable";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-   
+    
+    helix = {
+      url = "github:SoraTenshi/helix/new-daily-driver";
+      inputs.parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "fu";
+    };
     fu.url = "github:numtide/flake-utils";
     nix-gaming = {
       url = "github:fufexan/nix-gaming";
       inputs.flake-parts.follows = "flake-parts";
     };
   };
-
   outputs = { self, nixpkgs, home-manager, spicetify-nix, nixpkgs-f2k, ... } @inputs: 
     let
       system = "x86_64-linux"; 
