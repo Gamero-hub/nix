d29e5a39442312b334fef33e3391e4519358810d
Pablo <seguiaguilarpablo@gmail.com>
2023-07-12 20:07:22 +0200

    .

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-stable";
 
     home-manager = {
       url = github:nix-community/home-manager;
