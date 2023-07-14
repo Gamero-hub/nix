4585194ff3a3bb2542c5ec8384304cb553e084a1
Pablo <seguiaguilarpablo@gmail.com>
2023-07-14 15:37:45 +0200

    .

--- hosts/lowland/configuration.nix
+++ hosts/lowland/configuration.nix
@@ -49,19 +49,7 @@
 	};
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "es_ES.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_ES.UTF-8";
-    LC_IDENTIFICATION = "es_ES.UTF-8";
-    LC_MEASUREMENT = "es_ES.UTF-8";
-    LC_MONETARY = "es_ES.UTF-8";
-    LC_NAME = "es_ES.UTF-8";
-    LC_NUMERIC = "es_ES.UTF-8";
-    LC_PAPER = "es_ES.UTF-8";
-    LC_TELEPHONE = "es_ES.UTF-8";
-    LC_TIME = "es_ES.UTF-8";
-  };
+  i18n.defaultLocale = "en_US.UTF-8";
    
   services = {
     xserver = {
@@ -73,7 +61,5 @@
     };
   };
 
-  # Configure console keymap
-  console.keyMap = "es";
 
 }
