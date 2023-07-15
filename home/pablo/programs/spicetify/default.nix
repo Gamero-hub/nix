efd877c62a594132fa1adb77996f9b503c3d0f37
Pablo <seguiaguilarpablo@gmail.com>
2023-07-15 14:53:22 +0200

    .

--- home/pablo/programs/spicetify/default.nix
+++ home/pablo/programs/spicetify/default.nix
@@ -1,20 +1,28 @@
-{ inputs, spicetify-nix, ... }: {
-  # themable spotify
+{ pkgs, lib, spicetify-nix, ... }:
+let
+  spicePkgs = spicetify-nix.packages.${pkgs.system}.default;
+in
+{
+  # allow spotify to be installed if you don't have unfree enabled already
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "spotify"
+  ];
+
+  # import the flake's module for your system
+  imports = [ spicetify-nix.homeManagerModule ];
+
+  # configure spicetify :)
   programs.spicetify =
-    let
-      spicePkgs = spicetify-nix;
-    in
     {
       enable = true;
-
       theme = spicePkgs.themes.catppuccin-mocha;
-
       colorScheme = "flamingo";
 
       enabledExtensions = with spicePkgs.extensions; [
         fullAppDisplay
+        shuffle # shuffle+ (special characters are sanitized out of ext names)
         hidePodcasts
-        shuffle
       ];
     };
 }
+
