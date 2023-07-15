f97702162e6554badad669746672cb70ce9dde73
Pablo <seguiaguilarpablo@gmail.com>
2023-07-15 11:55:37 +0200

    .

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -64,7 +64,6 @@ in
       (nerdfonts.override { fonts = [ "Iosevka" "CascadiaCode" "JetBrainsMono" "Mononoki" "Monofur" "IBMPlexMono" ]; })
       noto-fonts-cjk
       noto-fonts-emoji
-      material-symbols
       noto-fonts
       noto-fonts-cjk
       noto-fonts-emoji
