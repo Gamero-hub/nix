d11d3608dfef17d4ccb64f50f00e0bc1e9ca4a33
Pablo <seguiaguilarpablo@gmail.com>
2023-07-15 13:29:43 +0200

    .

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -89,10 +89,10 @@ in
   };
 
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.extraConfig = "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1";
+  pulseaudio.enable = true; 
 
   environment.systemPackages = with pkgs; [
+    pulseaudio
     ####################
     pamixer
     imagemagick
