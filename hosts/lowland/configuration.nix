585ababfdc00d0de1d5263edb0640138e7580049
Pablo <seguiaguilarpablo@gmail.com>
2023-07-15 16:25:24 +0200

    .

--- hosts/lowland/configuration.nix
+++ hosts/lowland/configuration.nix
@@ -71,10 +71,6 @@
 
   # DWM
   nixpkgs.overlays = [
-    outputs.overlays.modifications
-    outputs.overlays.additions
-    inputs.nixpkgs-f2k.overlays.stdenvs
-    inputs.nixpkgs-f2k.overlays.compositors
     (final: prev:
       {
         awesome = inputs.nixpkgs-f2k.packages.${pkgs.system}.awesome-git;
