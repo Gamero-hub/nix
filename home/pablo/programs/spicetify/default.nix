af424c4bae266d930481c0da16f99589f3b001dd
Pablo <seguiaguilarpablo@gmail.com>
2023-07-15 14:57:52 +0200

    .

--- home/pablo/programs/spicetify/default.nix
+++ home/pablo/programs/spicetify/default.nix
@@ -1,28 +1,58 @@
-{ pkgs, lib, spicetify-nix, ... }:
+{wave, spicetify-nix,  pkgs, ...}:
 let
+  flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
+  spicetify-nix = (import flake-compat { src = builtins.fetchTarball "https://github.com/the-argus/spicetify-nix/archive/master.tar.gz"; }).defaultNix;
   spicePkgs = spicetify-nix.packages.${pkgs.system}.default;
+  
+
+  officialThemesOLD = pkgs.fetchgit {
+      url = "https://github.com/spicetify/spicetify-themes";
+      rev = "c2751b48ff9693867193fe65695a585e3c2e2133";
+      sha256 = "0rbqaxvyfz2vvv3iqik5rpsa3aics5a7232167rmyvv54m475agk";
+    };
+    localFilesSrc = pkgs.fetchgit {
+      url = "https://github.com/hroland/spicetify-show-local-files/";
+      rev = "1bfd2fc80385b21ed6dd207b00a371065e53042e";
+      sha256 = "01gy16b69glqcalz1wm8kr5wsh94i419qx4nfmsavm4rcvcr3qlx";
+    };
 in
 {
-  # allow spotify to be installed if you don't have unfree enabled already
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "spotify"
-  ];
-
-  # import the flake's module for your system
-  imports = [ spicetify-nix.homeManagerModule ];
-
-  # configure spicetify :)
-  programs.spicetify =
-    {
-      enable = true;
-      theme = spicePkgs.themes.catppuccin-mocha;
-      colorScheme = "flamingo";
-
-      enabledExtensions = with spicePkgs.extensions; [
-        fullAppDisplay
-        shuffle # shuffle+ (special characters are sanitized out of ext names)
+  imports = [spicetify-nix.homeManagerModule];
+  programs.spicetify = {
+    enable = true;
+    enabledExtensions = with spicePkgs.extensions; [
+        playlistIcons
+        spicetify-nix.packages.${pkgs.system}.default.extensions.adblock
+        genre
+        historyShortcut
         hidePodcasts
+        fullAppDisplay
+        shuffle
       ];
-    };
-}
+    enabledCustomApps = with spicePkgs.apps; [
+      lyrics-plus
+    ];
+      colorScheme = "custom";
+      theme = spicePkgs.themes.catppuccin-mocha;
+      customColorScheme = with wave;{
+        text = "${color7}";
+        subtext = "${color15}";
+        sidebar-text = "${color7}";
+        main = "${background}";
+        sidebar = "${mbg}";
+        player = "${bg2}";
+        card = "${color0}";
+        shadow = "${color8}";
+        selected-row = "${color8}";
+        button = "${color4}";
+        button-active = "${color4}";
+        button-disabled = "${color5}";
+        tab-active = "${color4}";
+        notification = "${color3}";
+        notification-error = "${color1}";
+        misc = "${comment}";
+      };
+      
 
+  };
+}
