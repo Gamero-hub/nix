017638c4f56442ad6c3170bf30aeef3ab165e13f
Pablo <seguiaguilarpablo@gmail.com>
2023-07-16 22:25:24 +0200

    .

--- home/pablo/home.nix
+++ home/pablo/home.nix
@@ -69,7 +69,7 @@ in
       (import ./programs/vscode { inherit pkgs config; })
       (import ./programs/neofetch { inherit config colors; })
     ];
-  home = {
+/*  home = {
     activation = {
       installConfig = ''
         if [ ! -d "${config.home.homeDirectory}/.config/awesome" ]; then
@@ -84,6 +84,7 @@ in
       '';
     };
   };
+  */
 
   # bat (cat clone)
   programs.bat = {
