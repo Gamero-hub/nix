bb0609e0982a96b723a7927deb4db607477d3a52
Pablo <seguiaguilarpablo@gmail.com>
2023-07-16 22:19:37 +0200

    .

--- home/pablo/home.nix
+++ home/pablo/home.nix
@@ -112,7 +112,7 @@ in
 	".config/dwm".source = ./cfg/dwm;
 	".config/helix".source = ./cfg/helix;
 	".bin".source = ./cfg/bin;
-	".dwm".source = ./cfg/dwm/autostart.sh;
+	".dwm".source = ./cfg/autostart.sh;
   };
 
 }
