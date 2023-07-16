796c1e4a388f93bb78ff8593cf7e57cb3213e3e2
Pablo <seguiaguilarpablo@gmail.com>
2023-07-16 21:48:25 +0200

    .

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     nix-gaming.url = "github:fufexan/nix-gaming";
   };
 
-  outputs = { self, nixpkgs, home-manager, spicetify-nix, nixpkgs-f2k, ... } @inputs:
+  outputs = { self, nixpkgs, home-manager, spicetify-nix, nixpkgs-f2k, neovim-flake, ... } @inputs:
     let
       inherit (self) outputs;
       system = "x86_64-linux";
