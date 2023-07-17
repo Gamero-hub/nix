e85f9aa33c0542e746eda226a49d4198c6a96d84
Pablo <seguiaguilarpablo@gmail.com>
2023-07-17 23:00:54 +0200

    .

--- home/pablo/home.nix
+++ home/pablo/home.nix
@@ -57,7 +57,7 @@ in
     [
       (import ./programs/rof/default.nix { inherit config pkgs wave; })
       (import ./programs/spicetify/default.nix { inherit wave spicetify-nix pkgs; })
-      (import ./programs/kitty { inherit colors pkgs; })
+#      (import ./programs/kitty { inherit colors pkgs; })
       (import ./programs/firefox { inherit pkgs config theme; })
       (import ./programs/bspwm { inherit pkgs; })
       (import ./programs/zsh/default.nix { inherit config pkgs; })
