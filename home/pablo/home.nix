d1253d07d0daec9f6c02f3de78f82bf229b5f911
Pablo Segui <seguiaguilarpablo@gmail.com>
2023-07-18 13:57:17 +0200

    .

--- home/pablo/home.nix
+++ home/pablo/home.nix
@@ -63,7 +63,7 @@ in
       (import ./programs/firefox { inherit pkgs config theme; })
       (import ./programs/bspwm { inherit pkgs; })
       (import ./programs/zsh/default.nix { inherit config pkgs; })
-      (import ./programs/git { inherit pkgs lib config; })
+#      (import ./programs/git { inherit pkgs lib config; })
       (import ./programs/vscode { inherit pkgs config; })
       (import ./programs/neofetch { inherit config colors; })
     ];
