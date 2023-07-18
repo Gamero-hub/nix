a6f819853d11e975d5f9caff6f37c08fa9469151
Pablo <seguiaguilarpablo@gmail.com>
2023-07-18 17:07:22 +0200

    .

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -15,8 +15,8 @@ in
       });
       st = prev.st.overrideAttrs (old: {
         src = /home/pablo/.config/suckless/st;
-        buildInputs = (old.buildInputs or []) ++ [pkgs.harfbuzz pkgs.xorg.libX11 pkgs.xorg.libXft pkgs.gd pkgs.glib pkgs.git];
-        nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.pkg-config pkgs.ncurses pkgs.fontconfig pkgs.freetype pkgs.git];
+        buildInputs = (old.buildInputs or []) ++ [pkgs.harfbuzz pkgs.xorg.libX11 pkgs.xorg.libXft pkgs.gd pkgs.glib;
+        nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.pkg-config];
       });
         dmenu = prev.st.overrideAttrs (old: {
                 src = /home/pablo/.config/suckless/dmenu;
