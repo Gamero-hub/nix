c43ad91f9607869028bae8291c7bc241db4a8d5f
Pablo <seguiaguilarpablo@gmail.com>
2023-07-18 14:46:28 +0200

    .

--- hosts/highland/configuration.nix
+++ hosts/highland/configuration.nix
@@ -76,7 +76,14 @@
         buildInputs = (old.buildInputs or []) ++ [pkgs.harfbuzz pkgs.xorg.libX11 pkgs.xorg.libXft pkgs.gd pkgs.glib pkgs.git];
         nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.pkg-config pkgs.ncurses pkgs.fontconfig pkgs.freetype pkgs.git];
       });
+	dmenu = prev.st.overrideAttrs (old: {
+        	src = /home/pablo/.config/suckless/dmenu;
+        	buildInputs = (old.buildInputs or []) ++ [pkgs.harfbuzz pkgs.xorg.libX11 pkgs.xorg.libXft pkgs.gd pkgs.glib ];
+        	nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.pkg-config pkgs.ncurses pkgs.fontconfig pkgs.freetype ];
+      });
+
 	awesome = inputs.nixpkgs-f2k.packages.${pkgs.system}.awesome-git;
+
     })
   ];
 
