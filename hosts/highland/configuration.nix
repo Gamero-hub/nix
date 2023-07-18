764be3b303863294dbe81895619b018b285cb17e
Tu Nombre <you@example.com>
2023-07-18 12:29:31 +0200

    .

--- hosts/highland/configuration.nix
+++ hosts/highland/configuration.nix
@@ -1,10 +1,22 @@
-{ inputs, outputs, config, pkgs, lib, self, ... }:
+{ config, pkgs, outputs, inputs, lib, self, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ../shared
     ];
+
+  nixpkgs = {
+    config = {
+      allowUnfreePredicate = _: true;
+      allowUnfree = true;
+    };
+  };
+
+  networking.hostName = "highland";
+  networking.networkmanager.enable = true;
 
   #Bootloader
   boot.loader.systemd-boot.enable = true;
@@ -12,18 +24,6 @@
   boot.loader.efi.efiSysMountPoint = "/boot";
   boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.timeout = 1;
-
-  hardware.opengl.driSupport32Bit = true;
-  programs.steam.enable = true;
-
-  #Networking
-  networking.hostName = "highland";  
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Madrid";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "es_ES.UTF-8";
@@ -40,65 +40,72 @@
     LC_TIME = "es_ES.UTF-8";
   };
 
-  # Enabling bluetooth
-  hardware = {
-    bluetooth.enable = true;
-  };
-
-  # Configure keymap in X11
   services = {
     xserver = {
-    displayManager.gdm.wayland = false;
-    layout = "us";
-    xkbVariant = "";
-    enable = true;
-    windowManager.dwm.enable = true;
-    displayManager.autoLogin.enable = true;
-    displayManager.autoLogin.user = "pablo";
-   }; 
+      enable = true;
+      layout = "us";
+      displayManager.sddm.enable = true;
+      windowManager.awesome = {
+        # Best window manager
+        enable = true;
+        luaModules = with pkgs.lua52Packages; [
+          lgi
+          ldbus
+          luarocks-nix
+          luadbi-mysql
+          luaposix
+        ];
+      };
+      windowManager.bspwm.enable = true;
+      windowManager.dwm.enable = true;
+    };
+  };
 
-   # enables blueman for bluetooth
-    blueman.enable = true;
- 
-   # automount usb
-    devmon.enable = true;
-    udisks2.enable = true;
- 
-};
   # Configure console keymap
   console.keyMap = "us";
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Dconf
-  programs.dconf.enable = true;
-
   # DWM
   nixpkgs.overlays = [
-  #  outputs.overlays.modifications
-  #  outputs.overlays.additions
-  #  inputs.nixpkgs-f2k.overlays.stdenvs
-    inputs.nixpkgs-f2k.overlays.compositors
-    (final: prev: 
-      {
-        dwm = prev.dwm.overrideAttrs (old: { src = /home/pablo/.config/suckless/dwm ;});
-      })
+    (final: prev: {
+      dwm = prev.dwm.overrideAttrs (old: {
+        src = /home/pablo/.config/suckless/dwm;
+        buildInputs = (old.buildInputs or []) ++ [pkgs.harfbuzz];
+        nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.pkg-config];
+      });
+      st = prev.st.overrideAttrs (old: {
+        src = /home/pablo/.config/st;
+        buildInputs = (old.buildInputs or []) ++ [pkgs.harfbuzz pkgs.xorg.libX11 pkgs.xorg.libXft pkgs.gd pkgs.glib pkgs.git];
+        nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.pkg-config pkgs.ncurses pkgs.fontconfig pkgs.freetype pkgs.git];
+      });
+	awesome = inputs.nixpkgs-f2k.packages.${pkgs.system}.awesome-git;
+    })
   ];
+/*  nixpkgs.overlays = [
+    (final: prev:
+      {
+        awesome = inputs.nixpkgs-f2k.packages.${pkgs.system}.awesome-git;
+      })
+
+    (final: prev:
+      {
+        dwm = prev.dwm.overrideAttrs (old: { src = /home/pablo/.config/suckless/dwm; });
+      })
+   (final: prev:
+      {
+   	st = prev.st.overrideAttrs (old: {
+        	src = /home/xenoxanite/Suckless/st;
+        	buildInputs = (old.buildInputs or []) ++ [pkgs.harfbuzz];
+        	nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.pkg-config];
+      });
+
+  ];*/
+
 
   # Nvidia
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
 
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-  # enable flakes
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = "experimental-features = nix-command flakes";
-   };  
-
-  nixpkgs.config.allUnfree = true; 
 
 
 }
