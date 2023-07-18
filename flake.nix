941f5a38deab2f3c57d3cf3f3b6ddb79b87a79b1
Pablo <seguiaguilarpablo@gmail.com>
2023-07-18 19:29:48 +0200

    .

--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
   outputs = { self, nixpkgs, home-manager, spicetify-nix, nixpkgs-f2k, ... } @inputs:
     let
       system = "x86_64-linux";
+      inherit (self) outputs;
       pkgs = nixpkgs.legacyPackages.${system};
       lib = nixpkgs.lib;
       overlays = with inputs; [
