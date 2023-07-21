e42c61a9928527b2ec383f011b10e21dfb6e4f90
Pablo <seguiaguilarpablo@gmail.com>
2023-07-21 17:33:16 +0200

    .

--- home/pablo/home.nix
+++ home/pablo/home.nix
@@ -59,9 +59,9 @@ in
       (import ./programs/bspwm { inherit pkgs; })
       (import ./programs/zsh/default.nix { inherit config pkgs; })
       (import ./programs/vscode { inherit pkgs config; })
-#      (import ./setup/hyprland {inherit pkgs;})
-#      (import ./programs/waybar {inherit config lib pkgs;})
-#      (import ./scripts {inherit config lib pkgs;})
+      (import ./setup/hyprland {inherit pkgs;})
+      (import ./programs/waybar {inherit config lib pkgs;})
+      (import ./scripts {inherit config lib pkgs;})
     ];
   home = {
     activation = {
