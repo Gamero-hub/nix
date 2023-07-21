af49c65492e96decc7150525a9874ef0becf48a0
Pablo <seguiaguilarpablo@gmail.com>
2023-07-21 16:33:41 +0200

    .

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -17,6 +17,12 @@
                 buildInputs = (old.buildInputs or []) ++ [pkgs.harfbuzz pkgs.xorg.libX11 pkgs.xorg.libXft pkgs.gd pkgs.glib pkgs.xorg.libXinerama];
                 nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.pkg-config pkgs.ncurses pkgs.fontconfig pkgs.freetype];
       });
+        waybar = prev.waybar.overrideAttrs (oldAttrs: {
+        	 mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+        	 postPatch = (oldAttrs.postPatch or "") + ''
+		    sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp'';
+      });
+
 
         awesome = inputs.nixpkgs-f2k.packages.${pkgs.system}.awesome-git;
 
