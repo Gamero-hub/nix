4539df7f490ab1df73bcf7eaeb431679aceeb820
Pablo <seguiaguilarpablo@gmail.com>
2023-07-21 12:42:01 +0200

    .

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -194,6 +194,13 @@
 
   programs.dconf.enable = true;
 
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+
   system = {
     stateVersion = "22.11";
   };
