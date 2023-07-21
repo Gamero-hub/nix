8ff4547549b13cd8b5db4f871d0da5709cb3acf4
Pablo <seguiaguilarpablo@gmail.com>
2023-07-21 17:34:32 +0200

    .

--- home/pablo/home.nix
+++ home/pablo/home.nix
@@ -60,7 +60,7 @@ in
       (import ./programs/zsh/default.nix { inherit config pkgs; })
       (import ./programs/vscode { inherit pkgs config; })
       (import ./setup/hyprland {inherit pkgs;})
-      (import ./programs/waybar {inherit config lib pkgs;})
+/*      (import ./programs/waybar {inherit config lib pkgs;})*/
       (import ./scripts {inherit config lib pkgs;})
     ];
   home = {
