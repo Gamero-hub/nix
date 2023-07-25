04db16ff7a52e26d4519e0068c5f8ea44839616f
Pablo <seguiaguilarpablo@gmail.com>
2023-07-25 14:51:29 +0200

    .

--- home/pablo/programs/zorro/default.nix
+++ home/pablo/programs/zorro/default.nix
@@ -209,7 +209,7 @@
 
             /* show nightly logo instead of default firefox logo in newtabpage */
             .search-wrapper .logo-and-wordmark .logo {
-                background: url("${./logo.png}") no-repeat center !important;
+           /*     background: url("${./logo.png}") no-repeat center !important;*/
                 background-size: auto !important;
                 background-size: 82px !important;
                 display: inline-block !important;
