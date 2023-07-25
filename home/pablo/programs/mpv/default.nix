c658a92bda4b3cf71f540067aab5b84fbca19732
Pablo <seguiaguilarpablo@gmail.com>
2023-07-25 16:10:55 +0200

    .

---
+++ home/pablo/programs/mpv/default.nix
@@ -0,0 +1,11 @@
+{ lib, pkgs, ... }:
+
+{
+  programs = {
+    mpv = {
+      enable = true;
+    };
+  };
+  home.file.".config/mpv/mpv.conf".source = ./mpv.conf;
+  home.file.".config/mpv/scripts/file-browser.lua".source = ./scripts/file-browser.lua;
+}
