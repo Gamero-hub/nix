7658f041fe71070af6d8f36e7bb7965c12bd3357
Pablo <seguiaguilarpablo@gmail.com>
2023-08-01 21:57:42 +0200

    .

--- home/pablo/home.nix
+++ home/pablo/home.nix
@@ -117,7 +117,7 @@ in
    
   home.sessionVariables = {
 #    LIBVA_DRIVER_NAME = nvidia;
-    XDG_SESSION_TYPE = wayland;
+#    XDG_SESSION_TYPE = wayland;
     GBM_BACKEND = nvidia-drm;
     __GLX_VENDOR_LIBRARY_NAME = nvidia;
     WLR_NO_HARDWARE_CURSORS = 1;
