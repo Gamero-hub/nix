0ec2596c22bebca1c45395d55622b1ae7b22257f
Pablo <seguiaguilarpablo@gmail.com>
2023-08-01 12:15:51 +0200

    .

--- home/pablo/programs/helix/default.nix
+++ home/pablo/programs/helix/default.nix
@@ -7,25 +7,6 @@
 
   programs.helix = {
     enable = true;
-    package = inputs.helix.packages.default.overrideAttrs (self: {
-      makeWrapperArgs = with pkgs;
-        self.makeWrapperArgs
-        or []
-        ++ [
-          "--suffix"
-          "PATH"
-          ":"
-          (lib.makeBinPath [
-            clang-tools
-            marksman
-            nil
-            nodePackages.bash-language-server
-            nodePackages.vscode-css-languageserver-bin
-            nodePackages.vscode-langservers-extracted
-            shellcheck
-          ])
-        ];
-    });
 
     settings = {
       theme = "catppuccin_mocha";
