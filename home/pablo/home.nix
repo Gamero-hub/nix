43dbe33fcfc4eed36acd72dab63c389e4390ca10
Pablo <seguiaguilarpablo@gmail.com>
2023-08-01 21:56:46 +0200

    .

--- home/pablo/home.nix
+++ home/pablo/home.nix
@@ -116,7 +116,13 @@ in
   };
    
   home.sessionVariables = {
+    LIBVA_DRIVER_NAME = nvidia;
+    XDG_SESSION_TYPE = wayland;
+    GBM_BACKEND = nvidia-drm;
+    __GLX_VENDOR_LIBRARY_NAME = nvidia;
     WLR_NO_HARDWARE_CURSORS = 1;
+
+
   };
     # headset buttons
   systemd.user.services.mpris-proxy = {
