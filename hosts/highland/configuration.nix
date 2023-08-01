8cbe4e001fb0fcdb178fbe908cebf7dd78940e14
Pablo <seguiaguilarpablo@gmail.com>
2023-08-01 22:31:35 +0200

    .

--- hosts/highland/configuration.nix
+++ hosts/highland/configuration.nix
@@ -94,7 +94,7 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
-  home-manager.users.xdg.configFile."hypr/hyprland.conf" = {
+  xdg.configFile."hypr/hyprland.conf" = {
     text = ''
      $mainMod = SUPER
      env = XCURSOR_SIZE,24
