ad9c6fa520d0162001f42ff05e2807f05afbd22c
Pablo <seguiaguilarpablo@gmail.com>
2023-08-07 23:59:33 +0200

    .

--- hosts/lowland/configuration.nix
+++ hosts/lowland/configuration.nix
@@ -30,6 +30,21 @@
     timeout = 1;
   };
 
+  i18n.defaultLocale = "ja_JP.UTF-8";
+  i18n.supportedLocales = ["es_ES.UTF-8", "ja_JP.UTF-8"];
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ja_JP.UTF-8";
+    LC_IDENTIFICATION = "ja_JP.UTF-8";
+    LC_MEASUREMENT = "ja_JP.UTF-8";
+    LC_MONETARY = "ja_JP.UTF-8";
+    LC_NAME = "ja_JP.UTF-8";
+    LC_NUMERIC = "ja_JP.UTF-8";
+    LC_PAPER = "ja_JP.UTF-8";
+    LC_TELEPHONE = "ja_JP.UTF-8";
+    LC_TIME = "ja_JP.UTF-8";
+  };
+
  i18n = {
     defaultLocale = "ja_JP.UTF-8/UTF-8";
     # saves space
