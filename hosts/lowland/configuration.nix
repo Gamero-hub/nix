8c25b68cdfb546ef2ded1cff6fd60a122c4526d2
Pablo <seguiaguilarpablo@gmail.com>
2023-08-07 23:48:41 +0200

    .

--- hosts/lowland/configuration.nix
+++ hosts/lowland/configuration.nix
@@ -34,7 +34,20 @@
     defaultLocale = "ja_JP.UTF-8/UTF-8";
     # saves space
     supportedLocales = [ "en_US.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8" ];
-  };
+
+  	extraLocaleSettings = {
+	    LC_ADDRESS = "ja_JP.UTF-8";
+	    LC_NAME = "ja_JP.UTF-8";
+	    LC_MONETARY = "ja_JP.UTF-8";
+	    LC_PAPER = "ja_JP.UTF-8";
+	    LC_IDENTIFICATION = "ja_JP.UTF-8";
+	    LC_TELEPHONE = "ja_JP.UTF-8";
+	    LC_MEASUREMENT = "ja_JP.UTF-8";
+	    LC_TIME = "ja_JP.UTF-8";
+	    LC_NUMERIC = "ja_JP.UTF-8";
+	    LANG = "ja_JP.UTF-8";
+     };
+    };
 
   services = {
     xserver = {
