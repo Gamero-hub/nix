43f8dc3366352c967c9b768798d1aebf468ab1a4
Pablo <seguiaguilarpablo@gmail.com>
2023-08-08 21:07:17 +0200

    .

--- hosts/highland/configuration.nix
+++ hosts/highland/configuration.nix
@@ -39,19 +39,21 @@
   boot.loader.systemd-boot.configurationLimit = 5;
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "es_ES.UTF-8";
+  i18n.defaultLocale = "ja_JP.UTF-8";
+  # i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" "en_US.UTF-8" ];
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_ES.UTF-8";
-    LC_IDENTIFICATION = "es_ES.UTF-8";
-    LC_MEASUREMENT = "es_ES.UTF-8";
-    LC_MONETARY = "es_ES.UTF-8";
-    LC_NAME = "es_ES.UTF-8";
-    LC_NUMERIC = "es_ES.UTF-8";
-    LC_PAPER = "es_ES.UTF-8";
-    LC_TELEPHONE = "es_ES.UTF-8";
-    LC_TIME = "es_ES.UTF-8";
-  };
+  # i18n.extraLocaleSettings = {
+  #  LC_ALL = "en_US.UTF-8";
+  #  LC_ADDRESS = "en_US.UTF-8";
+  #  LC_IDENTIFICATION = "en_US.UTF-8";
+  #  LC_MEASUREMENT = "en_US.UTF-8";
+  #  LC_MONETARY = "en_US.UTF-8";
+  #  LC_NAME = "en_US.UTF-8";
+  #  LC_NUMERIC = "en_US.UTF-8";
+  #  LC_PAPER = "en_US.UTF-8";
+  #  LC_TELEPHONE = "en_US.UTF-8";
+  #  LC_TIME = "en_US.UTF-8";
+  # };
 
   services = {
     xserver = {
