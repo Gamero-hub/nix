fd97f4f0b1ab912468af34c7a3112fc543b2d53b
Pablo <seguiaguilarpablo@gmail.com>
2023-08-08 11:09:04 +0200

    .

--- hosts/lowland/configuration.nix
+++ hosts/lowland/configuration.nix
@@ -30,20 +30,22 @@
     timeout = 1;
   };
 
+ja_JP.UTF-8 UTF-8
   # Select internationalisation properties.
-  i18n.defaultLocale = "es_ES.UTF-8";
+  i18n.defaultLocale = "ja_JP.UTF-8/UTF-8";
+  supportedLocales = [ "en_US.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8" ];
 
   i18n.extraLocaleSettings = {
-    LC_ALL = "es_ES.UTF-8";
-    LC_ADDRESS = "es_ES.UTF-8";
-    LC_IDENTIFICATION = "es_ES.UTF-8";
-    LC_MEASUREMENT = "es_ES.UTF-8";
-    LC_MONETARY = "es_ES.UTF-8";
-    LC_NAME = "es_ES.UTF-8";
-    LC_NUMERIC = "es_ES.UTF-8";
-    LC_PAPER = "es_ES.UTF-8";
-    LC_TELEPHONE = "es_ES.UTF-8";
-    LC_TIME = "es_ES.UTF-8";
+    LC_ALL = "ja_JP.UTF-8/UTF-8";
+    LC_ADDRESS = "ja_JP.UTF-8/UTF-8";
+    LC_IDENTIFICATION = "ja_JP.UTF-8/UTF-8";
+    LC_MEASUREMENT = "ja_JP.UTF-8/UTF-8";
+    LC_MONETARY = "ja_JP.UTF-8/UTF-8";
+    LC_NAME = "ja_JP.UTF-8/UTF-8";
+    LC_NUMERIC = "ja_JP.UTF-8/UTF-8";
+    LC_PAPER = "ja_JP.UTF-8/UTF-8";
+    LC_TELEPHONE = "ja_JP.UTF-8/UTF-8";
+    LC_TIME = "ja_JP.UTF-8/UTF-8";
   };
 
   services = {
