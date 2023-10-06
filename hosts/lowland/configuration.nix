e02094dcf14790892091ebe91db63b969ddb506e
Gamero_xD <seguiaguilarpablo@gmail.com>
2023-10-06 21:56:08 +0200

    .

--- hosts/lowland/configuration.nix
+++ hosts/lowland/configuration.nix
@@ -7,7 +7,7 @@
       ./hardware-configuration.nix
       ../shared
       ../../home/pablo/services/swaywm.nix
-      ../../home/pablo/home-manager/sway
+#      ../../home/pablo/home-manager/sway
     ];
 
   nixpkgs = {
