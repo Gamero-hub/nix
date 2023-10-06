10f24d661f6cee2942956634137e62ce62134dcd
Gamero_xD <seguiaguilarpablo@gmail.com>
2023-10-06 19:38:01 +0200

    .

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -37,20 +37,6 @@ in
     })
   ];
 
-    configure-gtk = pkgs.writeTextFile {
-    name = "configure-gtk";
-    destination = "/bin/configure-gtk";
-    executable = true;
-    text = let
-      schema = pkgs.gsettings-desktop-schemas;
-      datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-    in ''
-      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-      gnome_schema=org.gnome.desktop.interface
-      gsettings set $gnome_schema gtk-theme 'Dracula'
-    '';
-  };
-
   xdg.portal = {
     enable = true;
     wlr.enable = true;
