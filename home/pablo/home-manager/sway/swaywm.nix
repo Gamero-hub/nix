1f40850d997a36ffc2e66c40bea9c4d6a58177e1
Gamero_xD <seguiaguilarpablo@gmail.com>
2023-10-06 23:34:40 +0200

    .

--- home/pablo/home-manager/sway/swaywm.nix
+++ home/pablo/home-manager/sway/swaywm.nix
@@ -59,39 +59,33 @@ in
 #        resolution 1920x1080
 #    }
  
- programs.sway = {
-    enable = true;
-  };
-
-  wayland.windowManager.sway = {
-#    config = null;
-    extraConfig = ''
-
-    gaps inner 5
-    gaps outer 0
-
-    input "type:keyboard" {
-    xkb_layout es
-    }
-
-    input "type:pointer" {
-    accel_profile flat
-    pointer_accel 0.0
-    }
-
-    set $term kitty
-    set $mod mod4
-
-    bindsym $mod+Return exec $term
-    bindsym $mod+q kill
-    bindsym $mod+Shift+r reload
-    bindsym $mod+d exec pkill rofi || ~/.config/rofi/launcher.sh
-    bindsym $mod+Shift+space floating toggle
-
-    smart_gaps on
-
-    '';
-   };
+#    extraConfig = ''
+#
+#    gaps inner 5
+#    gaps outer 0
+#
+#    input "type:keyboard" {
+#    xkb_layout es
+#    }
+#
+#    input "type:pointer" {
+#    accel_profile flat
+#    pointer_accel 0.0
+#    }
+#
+#    set $term kitty
+#    set $mod mod4
+#
+#    bindsym $mod+Return exec $term
+#    bindsym $mod+q kill
+#    bindsym $mod+Shift+r reload
+#    bindsym $mod+d exec pkill rofi || ~/.config/rofi/launcher.sh
+#    bindsym $mod+Shift+space floating toggle
+#
+#    smart_gaps on
+#
+#    '';
+#   };
 
 
   environment.systemPackages = with pkgs; [
--- home/pablo/packages/default.nix
+++
@@ -1,4 +0,0 @@
-{ pkgs, inputs', ... }: {
-  configure-gtk = pkgs.callPackage ./configure-gtk/default.nix {};
-  dbus-sway-environment = pkgs.callPackage ./dbus-sway-environment {};
-}
