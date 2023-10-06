20fce5fe8e39c3246429389dcec92482eeb8be8d
Gamero_xD <seguiaguilarpablo@gmail.com>
2023-10-06 21:56:27 +0200

    .

--- hosts/lowland/configuration.nix
+++ hosts/lowland/configuration.nix
@@ -6,7 +6,7 @@
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../shared
-      ../../home/pablo/services/swaywm.nix
+#      ../../home/pablo/services/swaywm.nix
 #      ../../home/pablo/home-manager/sway
     ];
 
