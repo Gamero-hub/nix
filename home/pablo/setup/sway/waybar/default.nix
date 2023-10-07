80569458f6c6b3cd4526fdcd32766082ce556c83
Gamero_xD <seguiaguilarpablo@gmail.com>
2023-10-07 19:49:31 +0200

    .

--- home/pablo/setup/sway/waybar/default.nix
+++ home/pablo/setup/sway/waybar/default.nix
@@ -8,17 +8,11 @@
     enable = true;
   };
 
-  xdg.configFile = let
-    onChange = ''
-      ${pkgs.procps}/bin/pkill -u $USER -USR2 waybar || true
-    '';
-  in {
+  xdg.configFile = {
     "waybar/config" = {
-      inherit onChange;
       source = config.lib.file.mkOutOfStoreSymlink "${config.unsafeFlakePath}/modules/home-manager/waybar/config.json";
     };
     "waybar/style.css" = {
-      inherit onChange;
       source = config.lib.file.mkOutOfStoreSymlink "${config.unsafeFlakePath}/modules/home-manager/waybar/style.css";
     };
   };
